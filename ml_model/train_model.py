316ef542a8ac9febed4bf35156a58402ddf7975e
b0tish <maharjan.saugat1@gmail.com>
2025-06-25 13:26:19 +0545

    [+] added frontend option

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import spacy
 
-df = pd.read_csv("./data/Resume/Resume/Resume.csv")
-print(df.shape)
+# df = pd.read_csv("./data/Resume/Resume/Resume.csv")
+# print(df.shape)
+# print(df['Resume_html'][0])
+
+
